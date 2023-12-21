bbb681262e648edfde6cac384f6c64073a8a3e6b
Natzu83 <natzuac@yahoo.com>
2023-12-21 09:02:18 -0400

    First try

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,1 +1,31 @@
-Placeholder
+#!/usr/bin/python3
+"""
+Exercise 0:
+Interacts with the APIs
+at https://jsonplaceholder.typicode.com/
+and gets info about the users'
+to-do lists and which tasks have they completed.
+
+Head over to https://jsonplaceholder.typicode.com/todos/
+and https://jsonplaceholder.typicode.com/users
+to understand how the JSON data is recieved by this program!
+"""
+import requests
+
+if __name__ == "__main__":
+    from sys import argv
+
+    EMPLOYEE_ID = int(argv[1])
+
+    ROOT_URL = "https://jsonplaceholder.typicode.com/"
+
+    TODOS = requests.get(f'{ROOT_URL}todos/').json()
+    EMPLOYEE_NAMES = requests.get(f'{ROOT_URL}users/').json()
+
+    EMPLOYEE_NAME = EMPLOYEE_NAMES[EMPLOYEE_ID - 1]['name']
+
+    EMPLOYEE_TODOS = tuple(
+        task
+        for task in TODOS
+        if task['userId'] == EMPLOYEE_ID
+    )
