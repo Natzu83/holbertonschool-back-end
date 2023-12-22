6e5b78927db59ec513cb1b8f41f51d6e90c4db5f
Natzu83 <natzuac@yahoo.com>
2023-12-22 07:47:53 -0400

    Second try

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,46 +1,48 @@
 #!/usr/bin/python3
 """
-Exercise 0:
+Exercise 1:
 Interacts with the APIs
 at https://jsonplaceholder.typicode.com/
 and gets info about users'
-to-do lists and which tasks have they completed,
-treated as employees.
+to-do lists and which tasks have they completed.
+
+This file then writes that information into
+a file called f"{USER_ID}.csv".
 
 Go to https://jsonplaceholder.typicode.com/todos/
 and https://jsonplaceholder.typicode.com/users
 to understand how the JSON data is recieved by this program.
 """
+import csv
 import requests
 
 if __name__ == "__main__":
     from sys import argv
 
-    EMPLOYEE_ID = int(argv[1])
+    USER_ID = int(argv[1])
 
     ROOT_URL = "https://jsonplaceholder.typicode.com/"
 
     TODOS = requests.get(f'{ROOT_URL}todos/').json()
-    EMPLOYEE_NAMES = requests.get(f'{ROOT_URL}users/').json()
+    USERNAMES = requests.get(f'{ROOT_URL}users/').json()
 
-    EMPLOYEE_NAME = EMPLOYEE_NAMES[EMPLOYEE_ID - 1]['name']
+    USERNAME = USERNAMES[USER_ID - 1]['username']
 
-    EMPLOYEE_TODOS = tuple(
+    USER_TASKS = tuple(
         task
         for task in TODOS
-        if task['userId'] == EMPLOYEE_ID
+        if task['userId'] == USER_ID
     )
-    TOTAL_NUMBER_OF_TASKS = len(EMPLOYEE_TODOS)
 
-    DONE_TASKS_TITLES = tuple(
-        task['title']
-        for task in EMPLOYEE_TODOS
-        if task['completed'] is True
-    )
-    NUMBER_OF_DONE_TASKS = len(DONE_TASKS_TITLES)
+    with open(f"{USER_ID}.csv", "w") as output_file:
+        csv_writer = csv.writer(output_file, quoting=1)
 
-    print(f"Employee {EMPLOYEE_NAME} is done with\
- tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
-
-    for task_title in DONE_TASKS_TITLES:
-        print(f"\t {task_title}")
+        for task in USER_TASKS:
+            csv_writer.writerow(
+                (
+                    USER_ID,
+                    USERNAME,
+                    task['completed'],
+                    task['title']
+                )
+            )
